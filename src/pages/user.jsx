391c0b9d175acb13829fe8a677545af00397e2ff
HectorLedezma <hector.ledezma@alumnos.ucentral.cl>
2023-12-11 14:06:10 -0300

    plantilla para pagina de usuario

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,8 +1,11 @@
 import {react, useRef, useState, useEffect} from 'react';
+import { Link, Outlet, useNavigate} from "react-router-dom";
+import { conexion } from "../ConectionSQL/conexion";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import Heading from "./heading";
 import '../styles/profile.css'
 function UserProfile(){
-
-    let todoRef = useRef();
+    const navigate = useNavigate();
 
     const [windowSize, setWindowSize] = useState({
         width: window.innerWidth,
@@ -44,21 +47,52 @@ function UserProfile(){
     const [claseH,setClaseH] = useState(inicio)
     //console.log(localStorage.getItem('Tokken'))
     return(
-        <div ref={todoRef} className={responsivo(windowSize.width,windowSize.height,'0')[0]}>
-            <div className={responsivo(windowSize.width,windowSize.height,'1')[1]}>
-                <h2 className={responsivo(windowSize.width,windowSize.height,'2')[2]}>HOLA</h2>
-            </div>
-            <div className='pagina'>
-                <div className='UserBody'>
-                    <h1 onChange={
-                        ev=>{
-                            ev.preventDefault();
-                            console.log('Cambio')
-                        }
-                    }>{windowSize.width}x{windowSize.height}</h1>
+        <div id="page" className="site login-show">
+            
+            <div className="container">
+                <div className="wrappr">
+                    <div className="login">
+                        <Heading wel={
+                            (
+                                <h2>
+                                    Hola<br/> 
+                                    {"[Nombre de usuario]"}
+                                </h2>
+                            )
+                        } 
+                        logo1={<div/>} 
+                        logo2={<div/>}/>
+                        <div className="content-form">
+                            <div className="y-style">
+                                {/*Espacio para componentes */}
+                                <form action="" >
+                                    {/*Espacio para Inputs */}
+                                </form>
+                                {/*Espacio para componentes */}
+                                <div className="afterform">
+                                <div className='welcome'>
+                                        
+                                        <button 
+                                            onClick={
+                                                ev=>{
+                                                    /*Boton para volver*/
+                                                    ev.preventDefault();
+                                                    navigate('/');//eso redirige a la ruta especificada
+                                                }
+                                            }
+                                            onMouseOver={(e) => e.target.title = 'Volver'} 
+                                        id='BtnLogIn' className="GoBack centrado" type="sumbit">
+                                            <IoMdArrowRoundBack size={60}/>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
-                <div className='UserFoot'></div>
             </div>
+            <Outlet/>
         </div>
     );
 }
